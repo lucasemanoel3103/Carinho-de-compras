93d02beb20374995cf4396069059aa1470b74e9f
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2025-01-21 20:59:42 -0300

    Implementação de algumas funções no arquivo cart.js

--- shopee-cart/src/index.js
+++ shopee-cart/src/index.js
@@ -1,10 +1,21 @@
 import createItem from './services/item.js';
+import * as cartService from './services/cart.js';
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log("Welcome to yout shopee cart! "); 
 
 const item1 = await createItem("Fone de ouvido akg", 89.99, 1);
 const item2 = await createItem("Fone de ouvido tws", 19.99, 4);
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.deleteItem(myCart, item2.name);
+
+console.log("Shopee cart Total is:"); 
+await cartService.calculateTotal(myCart);
+
+
+
--- shopee-cart/src/services/cart.js
+++ shopee-cart/src/services/cart.js
@@ -1,15 +1,29 @@
-async function addItem(userCart) {
-
+async function addItem(userCart, item) {
+  userCart.push(item);
 }
 
-async function deleteItem(userCart, name){
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
 
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
 }
 
 async function removeItem(userCart, index) {
-    
+
 }
 
 async function calculateTotal(userCart) {
-    
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(result);
 }
+
+async function displayCart(userCart) {
+    console.log("Shopee cart list:");
+    userCart.array.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity} | Subtotal = ${item.subtotal()}`);
+    });
+}
+
+export { addItem, calculateTotal, deleteItem, removeItem, displayCart };
